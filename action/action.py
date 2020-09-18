8b7f49ee249a03cb072b3dafec1a8c7270fdfb30
Elias Oliveira <63745733+EliasOlie@users.noreply.github.com>
2020-09-17 23:23:58 -0300

    Commit to master

--- action/action.py
+++ action/action.py
@@ -1,17 +1,40 @@
-from ibm_watson import SpeechToTextV1, ApiException
-from ibm_cloud_sdk_core.authenticators import BasicAuthenticator
 from base64 import b64decode
 import speech_recognition as sr
 import json, os
 
 
-def main():
+def main(args):
     
     try:
+        _c_type, p_dict = parse_header(
+        args['__ow_headers']['content-type']
+        )
+        
+        # Decode body (base64)
+        decoded_string = b64decode(args['__ow_body'])
 
+        # Set Headers for multipart_data parsing
+        p_dict['boundary'] = bytes(p_dict['boundary'], "utf-8")
+        p_dict['CONTENT-LENGTH'] = len(decoded_string)
+        
+        # Parse incoming request data
+        multipart_data = parse_multipart(
+            BytesIO(decoded_string), p_dict
+        )
+
+        # Build flac file from stream of bytes
+        fo = open("audio_sample.flac", 'wb')
+        fo.write(multipart_data.get('audio')[0])
+        fo.close()
         # Read audio file and call Watson STT API:
+            with open(
+        os.path.join(
+            os.path.dirname(__file__), './.',
+            'audio_sample.flac'
+        ), 'rb'
+    ) as audio_file:
         r = sr.Recognizer()
-        file = sr.AudioFile('action/audio_sample.flac')
+        file = audio_file
         
         with file as source:
             audio = r.record(source)
